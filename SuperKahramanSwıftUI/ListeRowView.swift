b93b92123e3ae3b5598f907bbf7b282b81e2da14
metehannolgun <metehannolgun@gmail.com>
2025-05-28 17:13:42 +0300

    -Viewlar düzenlendi

---
+++ SuperKahramanSwıftUI/ListeRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ListeRowView.swift
+//  SuperKahramanSwıftUI
+//
+//  Created by Metehan Olgun on 28.05.2025.
+//
+
+import SwiftUI
+
+struct ListeRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListeRowView()
+}
